2504c1c501720069622c62aa210250a171829231
molokochris <molokochrisp742@gmail.com>
2022-12-08 15:54:04 -0800

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    res = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        else:
+            res += my_string[i]
+    return res
