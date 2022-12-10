2560ae4c2183f3dde6133dbde44a10ea740f9e83
molokochris <molokochrisp742@gmail.com>
2022-12-09 18:21:58 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-        return new_list
+    response = [num if num != search else replace for num in my_list]
+    return response
