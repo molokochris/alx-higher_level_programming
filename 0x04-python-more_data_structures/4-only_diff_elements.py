c19199d4d03c76a1770056e0405645d5221944bd
molokochris <molokochrisp742@gmail.com>
2022-12-09 18:37:37 -0800

    all

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    for i in set_1:
-        if i not in set_2:
-            return i
-        else:
-            continue
+    return set_1 ^ set_2
