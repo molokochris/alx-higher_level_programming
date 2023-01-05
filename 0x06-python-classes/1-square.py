aba0b0706f55efaed4e1247a0420218181e3e7a7
molokochris <molokochrisp742@gmail.com>
2023-01-04 17:57:09 -0800

    task 1.1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    _Square__size = 0
-
-    def __init__(self, sqr_size):
-        if sqr_size:
-            self._Square__size = sqr_size
+    def __init__(self, size):
+        self.__size = size
