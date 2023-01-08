bc1d04c8940b1bbf5d0bbbeaaaaa660b5162ff7c
molokochris <molokochrisp742@gmail.com>
2023-01-08 02:51:19 -0800

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-
+"""the following class defines a square."""
 class Square:
     """
     This is a class that represents a square.
     """
-
-
-    def __init__(self):
-        """
-        This is the constructor of the Square class.
-        """
-
-        pass
