3a5f540b9d0cf2c242ea6b07c07924f4a4caa860
molokochris <molokochrisp742@gmail.com>
2024-01-19 04:03:17 +0200

    compiled modules python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
     import sys
-    for i in range(len(dir(hidden_4))):
-        if (dir(hidden_4)[i][0] == '_' and dir(hidden_4)[i][1] == '_'):
-            continue
-        else:
-            print("{}".format(dir(hidden_4)[i]))
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        print(hidden_4)
