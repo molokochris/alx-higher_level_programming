d4f086241fbfcdc5e0eb2f4f561cfa96cbe7aa2c
molokochris <molokochrisp742@gmail.com>
2024-01-19 03:08:14 +0200

    hpful attmpts

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    length = len(sys.argv)
     a = 0
-    for i in range(1, len(sys.argv)):
+    for i in range(1, length):
         a += int(sys.argv[i])
-    print("{}".format(a))
+    print(a)
